9b366aa457c0c904d03491458fbb472cf4ad6284
adrian <adrian.adrianborquez@gmail.com>
2022-09-28 16:30:17 -0600

    Creación de estado 'PlayState' y se dibujo bird.png en pantalla.

---
+++ core/src/com/loal/flappybird/States/PlayState.java
@@ -0,0 +1,35 @@
+package com.loal.flappybird.States;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class PlayState extends State{
+    private Texture bird;
+
+    protected PlayState(GameStateManager gameStateManager) {
+        super(gameStateManager);
+        bird = new Texture("bird.png");
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch spriteBatch) {
+        spriteBatch.begin();
+        spriteBatch.draw(bird ,50,50);
+        spriteBatch.end();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
