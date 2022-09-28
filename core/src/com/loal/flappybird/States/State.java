75b8c600d62c209c5aec543a74f4812cb907c253
adrian <adrian.adrianborquez@gmail.com>
2022-09-28 13:33:23 -0600

    Creación de la clase 'GameStateManager' administrra todos lo estados del juego.

--- core/src/com/loal/flappybird/States/State.java
+++ core/src/com/loal/flappybird/States/State.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.math.Vector3;
 
 public abstract class State {
 
-    
+
     //MUESTRA SOLO UN PEDASO DEL VIDEOJUEGO...
     //MUESTRA SOLO LO QUE ESTE EN EL MOMENTO
     protected OrthographicCamera camera;
