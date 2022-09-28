523044c126f3cde3bf373a9fdcec1ba58d2fbe8f
adrian <adrian.adrianborquez@gmail.com>
2022-09-28 14:15:55 -0600

    Creación de la clase 'MenuSate' estado del menu de inicio que hereda de la clase abstracta 'Sate'.

---
+++ core/src/com/loal/flappybird/States/MenuState.java
@@ -0,0 +1,31 @@
+package com.loal.flappybird.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State{
+
+    
+    protected MenuState(GameStateManager gameStateManager) {
+        super(gameStateManager);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch spriteBatch) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
